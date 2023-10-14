39494700a43505e3e0e7794ee8593a844835f90e
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-10-14 16:17:36 -0300

    fix: :bug: force https in express

--- server/src/frameworks/express.js
+++ server/src/frameworks/express.js
@@ -3,10 +3,17 @@
 import express from "express";
 import cors from "cors";
 
-
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.enable("trust proxy");
+app.use(function (request, response, next) {
+  if (process.env.NODE_ENV != "development" && !request.secure) {
+    return response.redirect("https://" + request.headers.host + request.url);
+  }
+
+  next();
+});
 
 export default app;
