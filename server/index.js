2e493e2214e65a038da862ef8691526ee003dda8
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-10-14 16:44:48 -0300

    fix: :bug: force https in express

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 "use strict";
 
+import https from "https";
+import fs from "fs";
 import Router from "./router.js";
 
 const port = 3333;
 
-new Router().httpAdapter.listen(port);
+const app = new Router().httpAdapter.lib;
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("config/aws.pem"),
+      cert: fs.readFileSync("config/cert.pem"),
+    },
+    app
+  )
+  .listen(port);
--- server/src/frameworks/express.js
+++ server/src/frameworks/express.js
@@ -3,17 +3,20 @@
 import express from "express";
 import cors from "cors";
 
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.enable("trust proxy");
-app.use(function (request, response, next) {
-  if (process.env.NODE_ENV != "development" && !request.secure) {
-    return response.redirect("https://" + request.headers.host + request.url);
-  }
 
-  next();
-});
+
+// app.enable("trust proxy");
+// app.use(function (request, response, next) {
+//   if (process.env.NODE_ENV != "development" && !request.secure) {
+//     return response.redirect("https://" + request.headers.host + request.url);
+//   }
+
+//   next();
+// });
 
 export default app;
