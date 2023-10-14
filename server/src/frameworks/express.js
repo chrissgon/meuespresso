6709efa2c63dda33b03fa34380c09a5dfc3d8597
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-10-14 17:48:21 -0300

    fix: :bug: adjust certificates

--- server/src/frameworks/express.js
+++ server/src/frameworks/express.js
@@ -3,20 +3,9 @@
 import express from "express";
 import cors from "cors";
 
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-
-// app.enable("trust proxy");
-// app.use(function (request, response, next) {
-//   if (process.env.NODE_ENV != "development" && !request.secure) {
-//     return response.redirect("https://" + request.headers.host + request.url);
-//   }
-
-//   next();
-// });
 
 export default app;
