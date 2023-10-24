0937b54a1e21d9dfc18c2adabc62b8d3de42cd5c
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-10-23 23:16:38 -0300

    feat: :sparkles: remove force https

--- server/index.js
+++ server/index.js
@@ -1,19 +1,9 @@
 "use strict";
 
-import https from "https";
-import fs from "fs";
 import Router from "./router.js";
 
 const port = 3333;
 
 const app = new Router().httpAdapter.lib;
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("config/aws.pem"),
-      cert: fs.readFileSync("config/cert.pem"),
-    },
-    app
-  )
-  .listen(port);
+app.listen(port);
